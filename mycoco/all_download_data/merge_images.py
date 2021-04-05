90e26d00188ce40268dfcb44959382b4937c4b14
小屋 <73323076+wu-jianlong@users.noreply.github.com>
2021-04-05 22:33:37 +0800

    Update merge_images.py

--- mycoco/all_download_data/merge_images.py
+++ mycoco/all_download_data/merge_images.py
@@ -3,6 +3,8 @@ import shutil
 train_image_path = './mchar_train/'#下载好的存储路径
 val_image_path = './mchar_val/'
 dst_image_path = '../all_images/'#文件整合后的位置
+if not os.path.exists(dst_image_path):
+    os.makedirs(dst_image_path)
 train_image_list = os.listdir(train_image_path)
 val_image_list = os.listdir(val_image_path)
 for img in train_image_list:
